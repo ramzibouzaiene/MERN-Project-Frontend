0f2ee0d23a9e774689403d15e91d63f36560139d
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2022-10-23 13:35:33 +0100

    Add logic to the components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router , Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 
 function App() {
   return (
     <Router>
-    <div className="container">
-      Hello World
-    </div>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Routes>
+        <Route path="/" exact element={<ExercisesList />} />
+        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>exercises-list.component</div>
+    )
+  }
+}
+
